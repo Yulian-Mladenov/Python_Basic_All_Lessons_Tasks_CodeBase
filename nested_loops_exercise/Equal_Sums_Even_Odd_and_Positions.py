b8dea9d61a1e6e7b830aa9e3c5356126d10dd75a
Юлиан Младенов / Yulian Mladenov <yulian.mladenovv@gmail.com>
2025-01-16 16:03:31 +0100

    .

---
+++ nested_loops_exercise/Equal_Sums_Even_Odd_and_Positions.py
@@ -0,0 +1,15 @@
+number_1 = int(input())
+#number_2 = int(input())
+
+even_elements = 0
+odd_elements = 0
+equal_sum = 0
+
+for iteration in range(1, number_1 + 1):
+    #for index_sum_check in range(iteration):
+        # if index_sum_check % 2 == 0:
+        #     even_elements += index_sum_check
+        # elif index_sum_check % 2 != 0:
+        #     odd_elements += index_sum_check
+
+    print(iteration)
