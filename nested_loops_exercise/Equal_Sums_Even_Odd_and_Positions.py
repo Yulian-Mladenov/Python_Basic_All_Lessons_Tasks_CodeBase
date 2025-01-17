772332a024cf5e4755dc26736283a3d409b0b4f8
Юлиан Младенов / Yulian Mladenov <yulian.mladenovv@gmail.com>
2025-01-17 09:36:13 +0100

    .

--- nested_loops_exercise/Equal_Sums_Even_Odd_and_Positions.py
+++ nested_loops_exercise/Equal_Sums_Even_Odd_and_Positions.py
@@ -1,15 +1,34 @@
+# number_1 = int(input())
+# number_2 = int(input())
+#
+# even_elements = 0
+# odd_elements = 0
+# equal_sum = 0
+#
+# for iteration in range(number_1, number_2 + 1):
+#     #for index_sum_check in range(iteration):
+#         # if index_sum_check % 2 == 0:
+#         #     even_elements += index_sum_check
+#         # elif index_sum_check % 2 != 0:
+#         #     odd_elements += index_sum_check
+#
+#     print(iteration)
+
 number_1 = int(input())
-#number_2 = int(input())
+number_2 = int(input())
 
 even_elements = 0
 odd_elements = 0
 equal_sum = 0
 
-for iteration in range(1, number_1 + 1):
-    #for index_sum_check in range(iteration):
-        # if index_sum_check % 2 == 0:
-        #     even_elements += index_sum_check
-        # elif index_sum_check % 2 != 0:
-        #     odd_elements += index_sum_check
+for iteration in range(number_1, number_2 + 1):
+    for index_of_iteration in range(iteration):
+        if index_of_iteration % 2 == 0:
+            even_elements += index_of_iteration
+        else:
+            odd_elements += index_of_iteration
 
-    print(iteration)
+if even_elements == odd_elements:
+    print(number_1)
+else:
+    print('no match')
