2ba0fb3b2e66e6e83c8e66bcc1e24dbd1d5347dd
Юлиан Младенов / Yulian Mladenov <yulian.mladenovv@gmail.com>
2025-01-21 15:01:10 +0100

    Update Sum_from_Prime_numbers_and_sum_from_Non_Prime_numbers.py

--- nested_loops_exercise/Sum_from_Prime_numbers_and_sum_from_Non_Prime_numbers.py
+++ nested_loops_exercise/Sum_from_Prime_numbers_and_sum_from_Non_Prime_numbers.py
@@ -1,3 +1,32 @@
+
+# The prototype of my first approach
+# natural_number = 0
+# not_natural_number = 0
+#
+# while True:
+#     number = input()
+#     if number == "stop":
+#         break
+#     elif int(number) < 0:
+#         print("Number is negative.")
+#
+#     parsing_data = int(number)
+#     if parsing_data > 1:
+#         if parsing_data % 1 == 0 and parsing_data % parsing_data == 0:
+#             natural_number += parsing_data
+#
+#     elif parsing_data % 1 != 0 and parsing_data % parsing_data != 0:
+#         not_natural_number += parsing_data
+#
+#
+# print(f'"Sum of all prime numbers is: {natural_number}"')
+# print(f"Sum of all non prime numbers is: {not_natural_number}")
+
+
+# Second approach
+natural_number = 0
+not_natural_number = 0
+
 while True:
     number = input()
     if number == "stop":
@@ -5,18 +34,12 @@ while True:
     elif int(number) < 0:
         print("Number is negative.")
 
-    natural_number = 0
-    not_natural_number = 0
     parsing_data = int(number)
-    if parsing_data > 1:
-        if parsing_data % 1 == 0 and parsing_data % parsing_data == 0:
-            natural_number += parsing_data
+    if parsing_data % 1 == 0 or parsing_data % parsing_data == 0:
+        natural_number += parsing_data
 
-    elif parsing_data % 1 != 0 and parsing_data % parsing_data != 0:
+    else:
         not_natural_number += parsing_data
-
 
 print(f'"Sum of all prime numbers is: {natural_number}"')
 print(f"Sum of all non prime numbers is: {not_natural_number}")
-
-
