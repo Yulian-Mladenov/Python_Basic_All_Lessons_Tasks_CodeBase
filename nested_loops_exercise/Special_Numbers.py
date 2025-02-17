a0d2cd44c08dd53449fb54041e6a94d081bcaa93
Юлиан Младенов / Yulian Mladenov <yulian.mladenovv@gmail.com>
2025-02-17 22:50:58 +0100

    .

--- nested_loops_exercise/Special_Numbers.py
+++ nested_loops_exercise/Special_Numbers.py
@@ -1,9 +1,23 @@
+# my prototype
+# number = int(input())
+# concatenation =
+#
+# for special_number in range(1111, 10000):
+#     parsing_to_str = str(special_number) # parsing the number to str data type
+#     for string_number in parsing_to_str:
+#         str_to_int = int(string_number) # parsing again each element from string to integer
+#         if str_to_int == 0:
+#             break
+#         elif number % str_to_int == 0:
+#             concatenation =
+
+
 number = int(input())
 concatenation =
 
 for special_number in range(1111, 10000):
-    parsing_to_str = special_number # parsing the number to str data type
-    for string_number in range(parsing_to_str):
+    parsing_to_str = str(special_number) # parsing the number to str data type
+    for string_number in parsing_to_str:
         str_to_int = int(string_number) # parsing again each element from string to integer
         if str_to_int == 0:
             break
