8c807d8e8fc725893b2093b09b62462168b95501
Юлиан Младенов / Yulian Mladenov <yulian.mladenovv@gmail.com>
2025-02-19 12:58:26 +0100

    .

--- nested_loops_exercise/Special_Numbers.py
+++ nested_loops_exercise/Special_Numbers.py
@@ -11,15 +11,19 @@
 #         elif number % str_to_int == 0:
 #             concatenation =
 
+# here i stopped because it`s a moment to find out how to concatenate each element from the for loop in one string
+# and when all are collected to parse them in integer again or print them directly like a string.
+
+
 
 number = int(input())
-concatenation =
+magic_number_is_found = False
+
 
 for special_number in range(1111, 10000):
     parsing_to_str = str(special_number) # parsing the number to str data type
     for string_number in parsing_to_str:
-        str_to_int = int(string_number) # parsing again each element from string to integer
-        if str_to_int == 0:
+        if int(string_number) == 0:
+            magic_number_is_found = True
             break
         elif number % str_to_int == 0:
-            concatenation =
