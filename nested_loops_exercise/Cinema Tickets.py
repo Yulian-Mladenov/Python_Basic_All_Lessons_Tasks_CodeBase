690f49a85cc9fe9ff84118e5de684a7326dd7c91
Юлиан Младенов / Yulian Mladenov <yulian.mladenovv@gmail.com>
2025-02-24 22:56:07 +0100

    .

--- nested_loops_exercise/Cinema Tickets.py
+++ nested_loops_exercise/Cinema Tickets.py
@@ -7,6 +7,7 @@ while True:
     if movie_name == "Finish":
         finish_flag_indicator = True
         break
+
     ticket_quantity_per_movie = 0
     standard_ticket = 0
     student_ticket = 0
@@ -40,11 +41,13 @@ if finish_flag_indicator:
     print(f"{percentage_of_standard_ticket:.2f}% standard tickets.")
     print(f"{percentage_of_kid_tickets:.2f}% kids tickets.")
 
-# the result of the first movie is ok the result of the second movie return 36,00% instead of 100.00% -  that is not
-# correct. after input the keyword "Finish", instead of terminate the program and print four results, it continue to
-# expect input in the first test after Taxi movie the end-keyword is End. But on the second test with Scare Movie
+# the result of the first movie is ok, the result of the second movie return 36,00% instead of 100.00% -  that is not
+# correct. After input the keyword "Finish", instead of terminate the program and print four results, it continue to
+# expect input.
+# In the first test after Taxi movie the end-keyword is End,but on the second test with Scare Movie
 # movie, the end-keyword is Finish! Here is the hidden problem why my program cannot print the four variables after
-# keyword Finish is read.
+# keyword Finish is read and i don`t know why. Practically the key-word Finish is the next movie name
 
-# probably solutions: the place of the code blocks and variables in the scopes are not correct. Probably is
-# necessary to use flag variables to use the Finish statement to terminate the program and print the results.
+# probably solutions: the place of the code blocks and variables in the scopes are not correct. Probably is necessary
+# to use flag variables to use the Finish statement to terminate the program and print the results - is not working,
+# i tried.
