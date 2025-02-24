29334cc99b4a45d77eedd8c46efc5a72f71bdc57
Юлиан Младенов / Yulian Mladenov <yulian.mladenovv@gmail.com>
2025-02-24 07:10:11 +0100

    .

--- nested_loops_exercise/Cinema Tickets.py
+++ nested_loops_exercise/Cinema Tickets.py
@@ -1,9 +1,11 @@
 total_tickets_quantity = 0
+finish_flag_indicator = False
 
 while True:
     movie_name = input()
     empty_places_for_current_movie = int(input())
     if movie_name == "Finish":
+        finish_flag_indicator = True
         break
     ticket_quantity_per_movie = 0
     standard_ticket = 0
@@ -31,14 +33,18 @@ while True:
     percentage_of_standard_ticket = ((standard_ticket * empty_places_for_current_movie) / 100) * 100
     percentage_of_kid_tickets = ((kid_ticket * empty_places_for_current_movie) / 100) * 100
 
-print(f"Total tickets: {total_tickets_quantity}")
-print(f"{percentage_of_student_tickets:.2f}% student tickets.")
-print(f"{percentage_of_standard_ticket:.2f}% standard tickets.")
-print(f"{percentage_of_kid_tickets:.2f}% kids tickets.")
 
-# the result of the first movie is ok
-# the result of the second movie return 36,00% instead of 100.00% -  that is not correct.
-# after finish instead of terminate the program and print four results, it continue to expect input
+if finish_flag_indicator:
+    print(f"Total tickets: {total_tickets_quantity}")
+    print(f"{percentage_of_student_tickets:.2f}% student tickets.")
+    print(f"{percentage_of_standard_ticket:.2f}% standard tickets.")
+    print(f"{percentage_of_kid_tickets:.2f}% kids tickets.")
+
+# the result of the first movie is ok the result of the second movie return 36,00% instead of 100.00% -  that is not
+# correct. after input the keyword "Finish", instead of terminate the program and print four results, it continue to
+# expect input in the first test after Taxi movie the end-keyword is End. But on the second test with Scare Movie
+# movie, the end-keyword is Finish! Here is the hidden problem why my program cannot print the four variables after
+# keyword Finish is read.
 
 # probably solutions: the place of the code blocks and variables in the scopes are not correct. Probably is
 # necessary to use flag variables to use the Finish statement to terminate the program and print the results.
