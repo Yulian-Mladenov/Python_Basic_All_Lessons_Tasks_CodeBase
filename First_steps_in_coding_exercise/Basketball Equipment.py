96d734ec4ba3e9e5bd3fcfaae652c58dd2c48e49
Юлиан Младенов / Yulian Mladenov <yulian.mladenovv@gmail.com>
2025-05-21 18:49:43 +0200

    .

--- First_steps_in_coding_exercise/Basketball Equipment.py
+++ First_steps_in_coding_exercise/Basketball Equipment.py
@@ -3,11 +3,10 @@ anual_tax_for_training = int(input())
 bascketball_sneackers_price_in_percentage = 0.40
 basketball_suit_price_in_percentage = 0.20
 bascketball_ball_price_in_percentage = 0.25
-bascketball_accessories_price_in_percentage = 1/5
+bascketball_accessories_price_in_percentage = 1 / 5
 
-
-
-bascketball_sneakers_price = anual_tax_for_training - (anual_tax_for_training * bascketball_sneackers_price_in_percentage)
+bascketball_sneakers_price = anual_tax_for_training - (
+            anual_tax_for_training * bascketball_sneackers_price_in_percentage)
 bascketball_suit_price = bascketball_sneakers_price - (bascketball_sneakers_price * basketball_suit_price_in_percentage)
 basketball_ball_price = bascketball_suit_price - (bascketball_suit_price * bascketball_ball_price_in_percentage)
 bascketball_acesories_price = basketball_ball_price - (basketball_ball_price * 0.20)
