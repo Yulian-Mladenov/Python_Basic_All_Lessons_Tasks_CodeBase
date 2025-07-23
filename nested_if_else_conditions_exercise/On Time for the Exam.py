5bcecf7335deeb3a4467ef1d144a635c66321b3a
Юлиан Младенов / Yulian Mladenov <yulian.mladenovv@gmail.com>
2025-07-23 23:05:08 +0200

    .

--- nested_if_else_conditions_exercise/On Time for the Exam.py
+++ nested_if_else_conditions_exercise/On Time for the Exam.py
@@ -9,4 +9,16 @@ arriving_time_minutes = int(input())
 # we will convert both times - exam time and arriving time:
 converting_exam_time = exam_time_hour + exam_time_minutes
 converting_arriving_time = arriving_time_hour + arriving_time_minutes
-if converting_exam_time > converting_arriving_time:
+
+# if he came early with less than one hour:
+if arriving_time_minutes >= exam_time_minutes:
+    difference = arriving_time_minutes - exam_time_minutes
+    if difference <= 59:
+        if difference < 10:
+            print(f'0{difference} minutes before the start')
+        else:
+            print(f'{difference:.2f} minutes before the start')
+
+# if he came early with more than one hour:
+elif
+
