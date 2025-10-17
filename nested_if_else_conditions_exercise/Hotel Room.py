9d479737071617dce16d6d5fb001ec1bf34ba135
Юлиан Младенов / Yulian Mladenov <yulian.mladenovv@gmail.com>
2025-10-17 16:39:32 +0200

    .

--- nested_if_else_conditions_exercise/Hotel Room.py
+++ nested_if_else_conditions_exercise/Hotel Room.py
@@ -26,32 +26,26 @@ apartment_discount_over_14 = 0.10
 
 # prices variables:
 studio_price = 0
+final_studio_price = 0
 apartment_price = 0
+final_apartment_price = 0
 
 # the logic:
 if month == 'May' or month == 'October':
-
-    studio_price = 50
-    if number_of_the_nights > 14:
-        final_price_with_discount -= final_price_without_discount * 0.30
-    elif number_of_the_nights > 7:
-        final_price_with_discount -= final_price_with_discount * 0.05
-    apartment_price = 65
-
     studio_price = studio_price_MO * number_of_the_nights
     apartment_price = apartment_price_MO * number_of_the_nights
     if number_of_the_nights > 14:
         studio_price -= studio_price * discount_MO_over_14
         apartment_price -= apartment_price * apartment_discount_over_14
     elif number_of_the_nights > 7:
-        studio_price -= studio_price * discount_MO_over_7
+        studio_price -= studio_price_MO * discount_MO_over_7
 
 elif month == 'June' or month == 'September':
     studio_price = studio_price_JS * number_of_the_nights
     apartment_price = apartment_price_JS * number_of_the_nights
     if number_of_the_nights > 14:
         studio_price -= studio_price * discount_JS_over_14
-        apartment_price -= apartment_price_JS * apartment_discount_over_14
+        apartment_price -= apartment_price * apartment_discount_over_14
 
 if month == 'July' or month == 'August':
     studio_price = studio_price_JA * number_of_the_nights
@@ -59,19 +53,5 @@ if month == 'July' or month == 'August':
     if number_of_the_nights > 14:
         apartment_price -= apartment_price * apartment_discount_over_14
 
-if number_of_the_nights > 14:
-    final_price_with_discount -= final_price_without_discount * 0.05
-else:
-    final_price_without_discount = final_price_without_discount
-
-
-if number_of_the_nights > 14 and month == 'May' or month == 'October':
-    print(f"Apartment:{apartment_price * number_of_the_nights:.2f} lv.")
-    print(f"Studio: {studio_price * number_of_the_nights:.2f} lv.")
-
 print(f"Apartment: {apartment_price:.2f} lv.")
 print(f"Studio: {studio_price:.2f} lv.")
-
-
-
-
