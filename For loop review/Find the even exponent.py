5721578f5d3508a63d149eef7c98e781c1d63111
Юлиан Младенов / Yulian Mladenov <yulian.mladenovv@gmail.com>
2026-03-31 22:18:42 +0200

    .

---
+++ For loop review/Find the even exponent.py
@@ -0,0 +1,6 @@
+number = int(input())
+
+for index in range(number + 1):
+    if index % 2 == 0:
+        print(2 ** index)
+
---
+++ For loop review/Reading numbers from 1 to n.py
@@ -0,0 +1,4 @@
+N_number = int(input())
+
+for number in range(1, N_number + 1, 3):
+    print(number)
