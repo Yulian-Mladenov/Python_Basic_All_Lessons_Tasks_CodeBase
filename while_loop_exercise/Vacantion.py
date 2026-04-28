0de7f853ca78381013846349b783080589793ef0
Юлиан Младенов / Yulian Mladenov <yulian.mladenovv@gmail.com>
2026-04-28 17:34:25 +0200

    .

--- while_loop_exercise/Vacantion.py
+++ while_loop_exercise/Vacantion.py
@@ -7,22 +7,17 @@ spending_days_counter = 0
 while True:
     action = input()
     amount = float(input())
-    total_of_amount_variable = amount
     days_counter += 1
+
     if action == "spend":
         spending_days_counter += 1
+        # вади от available_cash, но не под 0
         available_cash -= amount
-        if spending_days_counter > 5:
-            print("You can't save the money.")
-            print(f"{days_counter}")
-            break
-    if available_cash - amount < 0:
-        available_cash = 0
-    else:
-        available_cash -= amount
+
+        # ако spending_days_counter == 5 → печатай и break
 
     if action == "save":
         spending_days_counter = 0
-    if total_of_amount_variable + available_cash == required_money_for_the_journey:
-        print(f"You saved the money for {days_counter} days.")
-        break
+        # добавя към available_cash
+
+    # ако available_cash >= required_money → печатай и break
